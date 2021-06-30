4e1e8e678aec391e390cfe0ccf9079ec9f6b6fce
Iñigo Basterretxea <41299431+acse-ib616@users.noreply.github.com>
2021-06-30 12:45:54 +0100

    Added point icons

--- mesh_convergence.m
+++ mesh_convergence.m
@@ -25,14 +25,14 @@ TR = [0.001096762300234,0.001151110756730,0.001197741821396,0.001211703000320,..
 % ylabel('1/DOFs'); xlabel('EB/LST');
 % title('Midspan vertical displacement');
 
-figure;
-plot(1./CST_dofs,Euler./CST);
-grid on;
-xlabel('1/DOFs'); ylabel('EB/CST');
-title('Midspan vertical displacement');
+% figure;
+% plot(1./CST_dofs,Euler./CST);
+% grid on;
+% xlabel('1/DOFs'); ylabel('EB/CST');
+% title('Midspan vertical displacement');
 
 figure;
-plot(1./TR_dofs,Euler./TR);
+plot(1./TR_dofs,Euler./TR,'*-');
 grid on;
 xlabel('1/DOFs'); ylabel('EB/TR');
 title('Midspan vertical displacement');
@@ -44,7 +44,7 @@ LST_dofs_A = [594,2210,8514,13202,20930,26082];
 LST_A = [0.00122808,0.00123029,0.00123223,0.00123285,0.0012329,0.00123291];
 
 figure;
-plot(1./LST_dofs,Euler./LST,1./LST_dofs_A,Euler./LST_A);
+plot(1./LST_dofs,Euler./LST,'*-',1./LST_dofs_A,Euler./LST_A,'o-');
 grid on;
 xlabel('1/DOFs'); ylabel('EB/LST');
 title('Midspan vertical displacement');
@@ -55,7 +55,7 @@ CST_dofs_A = [594,2210,5346,10302,20930];
 CST_A = [0.00106495,0.00118384,0.00120405,0.00121429,0.00122499];
 
 figure;
-plot(1./CST_dofs,Euler./CST,1./CST_dofs_A,Euler./CST_A);
+plot(1./CST_dofs,Euler./CST,'*-',1./CST_dofs_A,Euler./CST_A,'o-');
 grid on;
 xlabel('1/DOFs'); ylabel('EB/CST');
 title('Midspan vertical displacement');
